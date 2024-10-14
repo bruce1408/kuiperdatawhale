149f5d3a0ff2c0c144cb4897e88e57908f3db139
bruce1408 <summer56567@163.com>
2024-10-15 02:45:03 +0800

    feat: 添加了Tensor类的构造函数和析构函数，以及Tensor类的成员函数rows()、cols()、channels()、size()、set_data()、empty()、index()、at()、data()、slice()、at()、Padding()、Fill()、Show()、Flatten()、Rand()、Ones()、Transform()、raw_shapes()、raw_ptr()、raw_ptr()、values()、matrix_raw_ptr()、Reshape()。

--- course1/test/axby.cpp
+++ course1/test/axby.cpp
@@ -6,53 +6,57 @@
 #include <gtest/gtest.h>
 void Axby(const arma::fmat &x, const arma::fmat &w, const arma::fmat &b,
           arma::fmat &y) {
-  // 把代码写这里 完成y = w * x + b的运算
+    // 把代码写这里 完成y = w * x + b的运算
+    y = w * x + b;
 }
 
 TEST(test_arma, Axby) {
-  using namespace arma;
-  fmat w = "1,2,3;"
-           "4,5,6;"
-           "7,8,9;";
+    using namespace arma;
+    fmat w = "1,2,3;"
+            "4,5,6;"
+            "7,8,9;";
 
-  fmat x = "1,2,3;"
-           "4,5,6;"
-           "7,8,9;";
+    fmat x = "1,2,3;"
+            "4,5,6;"
+            "7,8,9;";
 
-  fmat b = "1,1,1;"
-           "2,2,2;"
-           "3,3,3;";
+    fmat b = "1,1,1;"
+            "2,2,2;"
+            "3,3,3;";
 
-  fmat answer = "31,37,43;"
+    fmat answer = "31,37,43;"
                 "68,83,98;"
                 "105,129,153";
 
-  fmat y;
-  Axby(x, w, b, y);
-  ASSERT_EQ(approx_equal(y, answer, "absdiff", 1e-5f), true);
+    fmat y;
+    Axby(x, w, b, y);
+    ASSERT_EQ(approx_equal(y, answer, "absdiff", 1e-5f), true);
 }
 
 void EPowerMinus(const arma::fmat &x, arma::fmat &y) {
-  // 把代码写这里 完成y = e^{-x}的运算
+    // 把代码写这里 完成y = e^{-x}的运算
+    y = arma::exp(-x);
 }
 
 TEST(test_arma, e_power_minus) {
-  using namespace arma;
+    using namespace arma;
 
-  fmat x(224, 224);
-  x.randu();
+    fmat x(224, 224);
+    x.randu();
 
-  fmat y;
-  EPowerMinus(x, y);
-  std::vector<float> x1(x.mem, x.mem + 224 * 224);
-  ASSERT_EQ(y.empty(), false);
-  for (int i = 0; i < 224 * 224; ++i) {
+    fmat y;
+    EPowerMinus(x, y);
+    std::vector<float> x1(x.mem, x.mem + 224 * 224);
+    ASSERT_EQ(y.empty(), false);
+    for (int i = 0; i < 224 * 224; ++i) {
     ASSERT_LE(std::abs(std::exp(-x1.at(i)) - y.at(i)), 1e-5f);
-  }
+    }
 }
 
 void Axpy(const arma::fmat &x, arma::fmat &Y, float a, float y) {
-  // 编写Y = a * x + y
+    // 编写Y = a * x + y
+    Y = a * x + y;
+
 }
 
 TEST(test_arma, axpy) {
--- course2/main.cpp
+++ course2/main.cpp
@@ -1,3 +1,10 @@
+/*
+ * @version: 1.0.0
+ * @Author: BruceCui
+ * @Date: 2024-10-09 16:25:15
+ * @LastEditors: BruceCui
+ * @LastEditTime: 2024-10-14 12:35:36
+ */
 //
 // Created by fss on 23-5-27.
 //
@@ -7,11 +14,11 @@
 
 #include <iostream>
 int main(int argc, char *argv[]) {
-  testing::InitGoogleTest(&argc, argv);
-  google::InitGoogleLogging("Kuiper");
-  FLAGS_log_dir = "../../course2/log";
-  FLAGS_alsologtostderr = true;
+    testing::InitGoogleTest(&argc, argv);
+    google::InitGoogleLogging("Kuiper");
+    FLAGS_log_dir = "../../course2/log";
+    FLAGS_alsologtostderr = true;
 
-  LOG(INFO) << "Start test...\n";
-  return RUN_ALL_TESTS();
+    LOG(INFO) << "Start test...\n";
+    return RUN_ALL_TESTS();
 }
